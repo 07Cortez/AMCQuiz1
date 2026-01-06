30a24604224c318abd695feabee0a61a56c1a719
Hassel <168712331+07Cortez@users.noreply.github.com>
2026-01-06 19:52:21 +0800

    Q1 Label and Comment the Flutter App Structure

--- main.dart
+++ main.dart
@@ -18,7 +18,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold( // #2 Scaffold: Ito naman ang nagbibigay ng basic layout structure, tulad ng lalagyan ng AppBar at body.
     appBar: AppBar(title: const Text('All Widgets')), // #3 AppBar: Ang toolbar sa itaas ng screen usually naglalaman ng title ng page.
     body: Center( // #7 Center: Ito ginagamit para mapunta sa gitna ng screen ang text nito. 
-      child: Container( // #8 Container: Parang isang box na ginagamit para lagyan ng padding, o margin ang isang widget.
+      child: Container( // #8 Container: Parang box na ginagamit para lagyan ng padding, o margin ang isang widget.
         padding: const EdgeInsets.all(20),
         child: Column( // #6 Column: Ito inaayos patayo or Vertical layout ang mga text or logo at images. 
           children: [
